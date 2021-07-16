358cc9384f8cecf8f140a1fa8e3e606d0cb1d040
Jezreel Joshua <jezreeljoshuam@gmail.com>
2021-07-16 18:59:58 +0800

    implement login

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,9 @@
+import { LEADING_TRIVIA_CHARS } from '@angular/compiler/src/render3/view/template';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { DataService } from '../services/data.service';
+import { UserService } from '../services/user.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +12,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  public loginForm: FormGroup
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private _router: Router,
+    private dataService: DataService,
+    private userService: UserService
+  ) { }
 
   ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required]],
+      password: ['', [Validators.required]],
+    })
+  }
+
+  ionViewDidEnter() {
+    if (this.userService.isLoggedIn()) {
+      this._router.navigate(['tabs'])
+    }
+  }
+
+  onSubmit($event) {
+    if (this.loginForm.invalid) {
+      alert('invalid')
+    } else {
+      this.dataService
+        .login(this.loginForm.value)
+        .subscribe((res: any) => {
+          if (res.data) {
+            this.userService.setUser(res.data)
+            this.loginForm.reset()
+            this.userService.setLoggedin()
+            this._router.navigate(['/tabs'])
+          } else {
+            alert('invalid creds')
+          }
+
+        })
+    }
+
   }
 
 }
