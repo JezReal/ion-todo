3a412050f58f1c8fbb0244a64b22bfa5c740a0e4
Jezreel Joshua <jezreeljoshuam@gmail.com>
2021-07-16 19:11:17 +0800

    implement logout

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
+import { UserService } from '../services/user.service';
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +10,35 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  constructor(
+    public alertController: AlertController,
+    private userService: UserService,
+    private router: Router
+  ) {}
 
+  async loadAlert() {
+    const alert = await this.alertController.create({
+      header: 'Logout',
+      message: 'Are you sure you want to log out?',
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'camcel',
+          handler: something => {
+            
+          }
+        },
+        {
+          text: 'Log out',
+          handler: something => {
+            this.userService.setLoggedOut()
+            this.userService.clearUser()
+            this.router.navigate(['/'])
+          }
+        }
+      ]
+    })
+
+    await alert.present()
+  }
 }
