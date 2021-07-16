9510008a270703054a6d3c20c2d78fee779577eb
Jezreel Joshua <jezreeljoshuam@gmail.com>
2021-07-16 22:26:12 +0800

    implement register

--- src/app/register/register.page.ts
+++ src/app/register/register.page.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ToastController } from '@ionic/angular';
+import { DataService } from '../services/data.service';
+import { UserService } from '../services/user.service';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +12,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterPage implements OnInit {
 
-  constructor() { }
+  public registerForm: FormGroup
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private dataService: DataService,
+    private toastController: ToastController
+  ) { }
 
   ngOnInit() {
+    this.registerForm = this.formBuilder.group({
+      email: ['', [Validators.required]],
+      password: ['', [Validators.required]],
+    })
+  }
+
+  onSubmit($event) {
+    if (this.registerForm.invalid) {
+      this.loadToast('Invalid', 1000)
+    } else {
+      this.dataService
+        .register(this.registerForm.value)
+        .subscribe((res: any) => {
+          if (res.data) {
+            this.loadToast('Registration successfull', 1000)
+            this.registerForm.reset()
+            this.router.navigate(['/login'])
+          } else {
+            this.loadToast('Registration failed', 1000)
+          }
+        })
+    }
+  }
+
+  async loadToast(message: string, length: number) {
+    const toast = await this.toastController.create({
+      message: message,
+      duration: length
+    })
+
+    toast.present()
   }
 
 }
