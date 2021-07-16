af953d8be5365d876ab52069100a2070e9dd6605
Jezreel Joshua <jezreeljoshuam@gmail.com>
2021-07-16 15:17:02 +0800

    setup service

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,9 +1,36 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
+  private baseUrl = 'http://localhost/todo/'
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+
+  login(data: any) {
+    return this.httpClient.post(this.baseUrl + "login", JSON.stringify(data));
+  }
+
+  register(data: any) {
+    return this.httpClient.post(this.baseUrl + "register", JSON.stringify(data));
+  }
+
+  getAllTodos(data: any) {
+    return this.httpClient.post(this.baseUrl + "todos", JSON.stringify(data));
+  }
+
+  delete(data: any) {
+    return this.httpClient.post(this.baseUrl + "deletetodo", JSON.stringify(data));
+  }
+
+  update(data: any) {
+    return this.httpClient.post(this.baseUrl + "updatetodo", JSON.stringify(data));
+  }
+
+  add(data: any) {
+    return this.httpClient.post(this.baseUrl + "inserttodo", JSON.stringify(data));
+
+  }
 }
