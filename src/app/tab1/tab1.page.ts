a9f0b294544ef6a245fdfda140c49db21e54380c
Jezreel Joshua <jezreeljoshuam@gmail.com>
2021-07-18 19:56:30 +0800

    implement showing todos

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { AlertController } from '@ionic/angular';
+import { AlertController, ModalController } from '@ionic/angular';
+import { DataService } from '../services/data.service';
 import { UserService } from '../services/user.service';
+import { TodomodalComponent } from '../todomodal/todomodal.component';
 
 @Component({
   selector: 'app-tab1',
@@ -10,11 +12,31 @@ import { UserService } from '../services/user.service';
 })
 export class Tab1Page {
 
+  todoList: any;
+  
+  todo: string = '';
+  todoDate: string = '';
+  todoId: any;
+
+  selectedTodo: any = {
+    id: -1,
+    date: null,
+    todo: null
+  }
+
+  showTip: boolean = false
+  
   constructor(
     public alertController: AlertController,
     private userService: UserService,
-    private router: Router
+    private router: Router,
+    private dataService: DataService,
+    private modalController: ModalController,
   ) {}
+
+  ionViewDidEnter() {
+    this.getAll()
+  }
 
   async loadAlert() {
     const alert = await this.alertController.create({
@@ -40,5 +62,45 @@ export class Tab1Page {
     })
 
     await alert.present()
+  }
+
+  async addTodo() {
+    const modal = await this.modalController.create({
+      component: TodomodalComponent,
+    })
+
+    modal.onDidDismiss().then(_ => {
+      this.getAll()
+    })
+    
+    return await modal.present()
+  }
+
+  async editTodo(todo: any) {
+    const modal = await this.modalController.create({
+      component: TodomodalComponent,
+      componentProps: {
+        'date': todo.date,
+        'todo': todo.todo
+      }
+    })
+
+    return await modal.present()
+  }
+
+  getAll() {
+    this.dataService
+      .getAllTodos({
+        account_id: this.userService.getUser().userId,
+      })
+      .subscribe((res: any) => {
+        if (res.data) {
+          this.todoList = res.data;
+          this.showTip = false
+        } else if (res.error) {
+          this.showTip = true
+          this.todoList = null
+        }
+      });
   }
 }
